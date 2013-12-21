2e931946e5faa2f0434846f0b9c15c2eb85baf31
Max Mays <cheeser12@ezen.com>
2013-12-20 23:04:56 -0500

    Add command line options to designate input/output files, and add prompt for overwriting an existing database

--- mdb_to_sqlite.pl
+++ mdb_to_sqlite.pl
@@ -1,15 +1,60 @@
+#!/usr/bin/perl -w
 use strict;
 use warnings;
+use Getopt::Long;
 
-my $mdb_name = "flanders.mdb";
-my $outdb_name = "flanders.db";
+my $auto_overwrite = '';
+my $help = '';
+GetOptions ('auto-overwrite!' => \$auto_overwrite,
+            'help!' => \$help);
 
-# Generate the schema
+# Make sure we have two arguments
+my $num_args = $#ARGV + 1;
+if ($num_args != 2 || $help) {
+    print "Wrong number of arguments (got $num_args, need 2)\n" 
+      if (!$help);
+    print "Usage: mdb_to_sqlite.pl [--auto-overwrite] MDB_FILE OUT_FILE\n";
+    exit;
+}
+my ($mdb_name, $outdb_name) = @ARGV;
+
+# If the file exists...
+if (-e $outdb_name) {
+    if ($auto_overwrite) {
+        print "$outdb_name will be overwritten.\n";
+        `rm $outdb_name`;
+    }
+    else {
+        my $overwrite;
+
+        # Ask user whether they want to overwrite the file
+        # Keep looping until we get a valid answer
+        while (1) {
+            print "$outdb_name exists! Overwrite? (Y/N) ";
+            $overwrite = <STDIN>;
+
+            last if $overwrite =~ m/^(y|n)$/i;
+
+            print "Didn't understand input...\n";
+        }
+
+        if ($overwrite =~ m/^y$/i) {
+            print "$outdb_name will be overwritten.\n";
+            `rm $outdb_name`;
+        }
+        elsif ($overwrite =~ m/^n$/i) {
+            print "Aborting...\n";
+            exit;
+        }
+    }
+}
+
+# Generate the schema, initializing the database
 print "Generating schema...\n";
 `mdb-schema $mdb_name | sqlite3 $outdb_name`;
 print "Schema has been generated.\n";
 
-# Copy data from tables
+# Export the data from each table
 my @tables = split "\n", `mdb-tables -1 $mdb_name`;
 for (@tables) {
     print "Exporting data from table $_...\n";
